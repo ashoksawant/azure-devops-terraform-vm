6d050ca843354748ec347a53a36b3a03195db618
Sawant <ashok.a.sawant@capgemini.com>
2025-02-19 17:36:22 +0530

    add varaibale

--- variables.tf
+++ variables.tf
@@ -1,10 +1,15 @@
 variable "subscription_id" {
+  type        = string
 }
 variable "client_id" {
+  type        = string
 }
 variable "tenant_id" {
+  type        = string
 }
 variable "client_secret" {
+  type        = string
+  sensitive   = true
 }
 
 variable "storage_account_name" {
