d81c533fc497f95e54359e76d846b5eab74aea51
Sawant <ashok.a.sawant@capgemini.com>
2025-02-19 16:42:50 +0530

    changes move to hub

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "1-434cb672-playground-sandbox"
-    storage_account_name  = "azuredevopslabstrg"
-    container_name        = "container1"
-    key                   = "terraform.tfstate"
-  }
-}
